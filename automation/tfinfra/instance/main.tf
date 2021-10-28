62a68f7962585549ecbc8f88f6bc0d25520b78d4
Joe-Degs <jpedegs8990@gmail.com>
2021-10-28 18:52:07 +0100

    correct format

--- automation/tfinfra/instance/main.tf
+++ automation/tfinfra/instance/main.tf
@@ -1,26 +1,25 @@
-variable "instance_name" {  }
-variable "instance_zone" {  }
+variable "instance_name" {}
+variable "instance_zone" {}
 variable "instance_type" {
-    default = "n1-standard-1"
+  default = "n1-standard-1"
 }
 
 resource "google_compute_instance" "vm_instance" {
-    name = var.instance_name
-    zone = var.instance_zone
-    type = var.instance_type
+  name = var.instance_name
+  zone = var.instance_zone
+  type = var.instance_type
 
-    boot_disk {
-        initialize_params {
-            image = "debian-cloud/debian-9"
-        }
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
     }
+  }
 
-    network_interface {
-        network = var.instance_network
+  network_interface {
+    network = var.instance_network
 
-        access_config {
-            # allocate a one-to-one NAT IP to the instance
-
-        }
+    access_config {
+      # allocate a one-to-one NAT IP to the instance
     }
+  }
 }
